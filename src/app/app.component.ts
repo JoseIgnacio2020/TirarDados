e8e067b2a9ed1bf40fdbe0f16e80f02acf6e4ac7
JoseIgnacio2020 <jose.ignacio.silva2015@gmail.com>
2023-07-14 17:25:08 -0400

    Se desarrolla aplicación que lanza dados y al coincidir ambas caras de los dados, muestra mensaje de ganaste.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dados';
+  dadoIzquierdo = "../assets/img/dice1.png";
+  dadoDerecho = "../assets/img/dice4.png";
+  numero1 = 1;
+  numero2 = 2;
+
+  TirarDados(): void {
+    this.numero1 = Math.round(Math.random() * 5) + 1 ;
+    this.numero2 =  Math.round(Math.random() * 5) + 1;
+    this.dadoIzquierdo = "../assets/img/dice" + this.numero1 + ".png";
+    this.dadoDerecho = "../assets/img/dice" + this.numero2 + ".png";
+
+
+  }
 }
